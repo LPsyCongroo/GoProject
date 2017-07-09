048fbb41f35f25ba6f14aca3f8905430253cacfe
Ali Rehmatullah <rehmatullah.ali@gmail.com>
2017-07-09 17:08:09 -0400

    Initial code dump. A basic board of adjustable size.

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict"
+
+const boardSize = 19;
+
+const board = document.createElement("div");
+board.className="board";
+
+document.body.appendChild(board);
+
+for(let i = 0; i < boardSize; i++){
+  const row = document.createElement("div");
+  row.className="row";
+  let currentRow = document.querySelector(".board").appendChild(row);
+  // let currentRow = document.querySelector(`.row:nth-of-type(0n + ${i+1})`);
+  for(let j = 0; j < boardSize; j++){
+    const square = document.createElement("div");
+    square.className="square";
+    let currentSquare = currentRow.appendChild(square);
+    // const piece = document.createElement("div");
+    // piece.className="piece";
+    // currentSquare.appendChild(piece);
+  }
+}
